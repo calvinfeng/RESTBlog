fac4ead95d9a98ae04d9e5b884cb7136ed9c4bcb
Calvin Feng <cfeng@fetchrobotics.com>
2017-08-17 14:18:57 -0700

    Added permissions to views

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,3 +1,10 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter
+)
+
 from rest_framework.generics import (
     ListAPIView,
     RetrieveAPIView,
@@ -6,29 +13,51 @@ from rest_framework.generics import (
     CreateAPIView,
     )
 
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly
+    )
+
 from posts.api.serializers import (
     PostListSerializer,
     PostDetailSerializer,
-    PostCreateUpdateSerializer,
+    PostCreateUpdateSerializer
     )
 
+from posts.api.permissions import IsOwnerOrReadOnly
 from posts.models import Post
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            queryset_list = queryset_list.filter(
+                    Q(title__icontains=query) |
+                    Q(content__icontains=query) |
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query)
+                    ).distinct()
+        return queryset_list
 
 class PostCreateAPIView(CreateAPIView):
     query = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated]
 
     # def perform_create(self, serializer):
     #     serializer.save(user=self.request.user)
 
-
 class PostUpdateAPIView(RetrieveUpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostCreateUpdateSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
     # lookup_field = 'slug'
 
     # def perform_update(self, serializer):
