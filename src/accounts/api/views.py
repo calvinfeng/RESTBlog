8faacdc036cc690c33218afcc48a3ee001ce67ff
Calvin Feng <cfeng@fetchrobotics.com>
2017-08-17 19:12:47 -0700

    Completed user login API

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -1,17 +1,12 @@
 from django.db.models import Q
 from django.contrib.auth import get_user_model
 
-
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
-from rest_framework.views import APIView
-
-from rest_framework.filters import (
-        SearchFilter,
-        OrderingFilter,
-    )
-
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.mixins import DestroyModelMixin, UpdateModelMixin
+
+from rest_framework.views import APIView
 from rest_framework.generics import (
     CreateAPIView,
     DestroyAPIView,
@@ -21,17 +16,24 @@ from rest_framework.generics import (
     RetrieveUpdateAPIView
     )
 
-from rest_framework.permissions import (
-    AllowAny,
-    IsAuthenticated,
-    IsAdminUser,
-    IsAuthenticatedOrReadOnly,
-    )
-
-from .serializers import UserCreateSerializer
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
+from .serializers import UserCreateSerializer, UserLoginSerializer
 
 User = get_user_model()
 
 class UserCreateAPIView(CreateAPIView):
+    permission_classes = [AllowAny]
     serializer_class = UserCreateSerializer
     queryset = User.objects.all()
+
+class UserLoginAPIView(APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        serialized_data = UserLoginSerializer(data=data)
+        if serialized_data.is_valid(raise_exception=True):
+            new_data = serialized_data.data
+            return Response(new_data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
